a7fe5ce11a6e1016f4013be0b40cf2b474656992
我自己 <max@wufandemacbook-pro.local>
2018-10-31 16:44:38 +0800

    Initial Commit

---
+++ UILocationNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILocationNotification
+//
+//  Created by 吴凡 on 2018/10/31.
+//  Copyright © 2018 吴凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UILocationNotification/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UILocationNotification
+//
+//  Created by 吴凡 on 2018/10/31.
+//  Copyright © 2018 吴凡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
